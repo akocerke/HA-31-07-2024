f228980034175cf5bb5214b01c5fedb045ca3fbd
akocerke <152269613+akocerke@users.noreply.github.com>
2024-07-31 14:55:35 +0200

    #16

--- ec2/main.tf
+++ ec2/main.tf
@@ -18,3 +18,24 @@ resource "aws_subnet" "my_subnet" {
     Name = "my_subnet"
   }
 }
+
+
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow inbound traffic for web access"
+  vpc_id      = var.vpc_id  # Stellen Sie sicher, dass vpc_id korrekt ist
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
