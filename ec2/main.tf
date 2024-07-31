98e4cc7bb40a028f90f68c5602b513ef2d5a35fe
akocerke <152269613+akocerke@users.noreply.github.com>
2024-07-31 14:42:49 +0200

    #12

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "my_instance" {
-  ami           = "ami-123456"
+  ami           = "ami-060f408ac4bbdc3cf"  # Die korrekte AMI-ID
   instance_type = "t2.micro"
   
-  # Weitere Konfigurationen...
+  # Weitere Konfigurationen
 }
+
 
 
