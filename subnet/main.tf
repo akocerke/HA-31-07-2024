b322a557ccf3b7bc04d8c64c1338b7c29418391c
akocerke <152269613+akocerke@users.noreply.github.com>
2024-07-31 14:32:01 +0200

    #10

--- subnet/main.tf
+++ subnet/main.tf
@@ -1,8 +1,16 @@
+# Example input variable for VPC ID
+variable "vpc_id" {
+  description = "The ID of the VPC where the subnet will be created."
+  type        = string
+}
+
+# Subnet Resource
 resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = var.vpc_id
   cidr_block        = "10.0.1.0/24"
-  
-  # Weitere Konfigurationen...
+  availability_zone = "eu-central-1a"
+
+  # Optional: Tags, etc.
 }
 
 
